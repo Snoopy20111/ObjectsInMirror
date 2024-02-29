ccd831f93ee5037fc24700a709cb156d5e39362b
Snoopy20111 <snoopy20111@gmail.com>
2024-02-28 22:25:35 -0600

    Initial Commit
    Mostly-blank projects in Wwise and Godot

---
+++ GodotProj/GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,62 @@
+class_name AK
+
+class EVENTS:
+
+	const PLAY_TEST = 3187507146
+
+	const _dict = {
+		"Play_Test": PLAY_TEST
+	}
+
+class STATES:
+
+	const _dict = {}
+
+class SWITCHES:
+
+	const _dict = {}
+
+class GAME_PARAMETERS:
+
+	const _dict = {}
+
+class TRIGGERS:
+
+	const _dict = {}
+
+class BANKS:
+
+	const INIT = 1355168291
+	const MASTER = 4056684167
+
+	const _dict = {
+		"Init": INIT,
+		"Master": MASTER
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS = 3803692087
+
+	const _dict = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict = {}
+
+class AUDIO_DEVICES:
+
+	const NO_OUTPUT = 2317455096
+	const SYSTEM = 3859886410
+
+	const _dict = {
+		"No_Output": NO_OUTPUT,
+		"System": SYSTEM
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict = {}
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ObjectsInMirror
  ...may be closer than they appear. A Top-down driving game about getting back to civilization, away from the woods, and the shadows that lurk within.
+
+Running on Godot 4.2.1 and using Wwise 2023.1.
