b0531e6dc19583cdca1bab77e7977ee9bf340d1c
Snoopy20111 <snoopy20111@gmail.com>
2024-03-01 16:51:43 -0600

    Logo Parade - Name Reveal
    A simple animation addition, because I'm avoiding the bigger issue of making a game

---
+++ GodotProj/Scripts/LogoParade_PathFollow2D.gd
@@ -0,0 +1,22 @@
+extends PathFollow2D
+
+@export var speed_mult:float = 0.08
+const progress_curve = preload("res://Curves/LogoParade_wordFadeCurve.tres")
+
+var is_running:bool = false
+var counter_value:float = 0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (is_running):
+		counter_value += (delta * speed_mult)
+		progress_ratio += progress_curve.sample(counter_value)
+		if (counter_value >= 1):
+			is_running = false
+	pass
+
+#fired from signal to start motion
+func _on_wordstimer_timeout():
+	print("fired")
+	is_running = true
+	counter_value = 0
