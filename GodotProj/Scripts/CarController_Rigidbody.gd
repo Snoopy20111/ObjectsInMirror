411cc2eccb0f0bdab10448b4c4ec0684c12d982e
Snoopy20111 <snoopy20111@gmail.com>
2024-03-01 19:40:18 -0600

    Rigidbody Car (working now)
    It's certainly functional...now that I know how to work with this stuff though, I'm probably going to port in the unity-style code and see what I can do from there.

--- GodotProj/Scripts/CarController_Rigidbody.gd
+++ GodotProj/Scripts/CarController_Rigidbody.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 @export_group("Basic Car properties")
 @export var wheel_base:float = 50
 @export var steering_angle:float = 25
-@export var engine_power:float = 2000
+@export var engine_power:float = 900
 @export var braking_power:float = -450
 @export var max_speed_reverse:float = 250
 
@@ -13,24 +13,26 @@ extends RigidBody2D
 @export var steering_mult:float = 10
 @export var traction_curve:Curve = preload("res://Curves/Car_XSpeedYTraction_Default.tres")
 
-var velocity:Vector2 = Vector2.ZERO
+#var velocity:Vector2 = Vector2.ZERO
 var acceleration:Vector2 = Vector2.ZERO
 var steer_direction:float
 var last_heading:Vector2 = Vector2.ZERO
 
-func _physics_process(delta):
+
+func _integrate_forces(state):
 	acceleration = Vector2.ZERO
 	get_input()
 	apply_friction()
-	calculate_steering(delta)
-	velocity += acceleration * delta
+	calculate_steering(state.step)
+	apply_central_impulse(acceleration)
+	#linear_velocity += acceleration * state.step
 	#move_and_slide()
 
 func apply_friction():
-	if velocity.length() < 5:
-		velocity = Vector2.ZERO
-	var friction_force = velocity * friction
-	var drag_force = velocity * velocity.length() * drag
+	if linear_velocity.length() < 5:
+		linear_velocity = Vector2.ZERO
+	var friction_force = linear_velocity * friction
+	var drag_force = linear_velocity * linear_velocity.length() * drag
 	acceleration += drag_force + friction_force
 
 func get_input():
@@ -39,8 +41,6 @@ func get_input():
 		turn += 1
 	if Input.is_action_pressed("drive_left"):
 		turn -= 1
-	if sign(turn) != sign(steer_direction):
-		turn *= 1.5
 	steer_direction = turn * deg_to_rad(steering_angle)
 	if Input.is_action_pressed("drive_forward"):
 		acceleration = transform.x * engine_power
@@ -50,24 +50,22 @@ func get_input():
 func calculate_steering(delta):
 	var rear_wheel = position - transform.x * wheel_base/2.0
 	var front_wheel = position + transform.x * wheel_base/2.0
-	rear_wheel += velocity * delta
-	front_wheel += velocity.rotated(steer_direction) * delta
+	rear_wheel += linear_velocity * delta
+	front_wheel += linear_velocity.rotated(steer_direction) * delta
 	var new_heading = (front_wheel - rear_wheel).normalized()
 	
-	var traction = traction_curve.sample(velocity.x / engine_power)
-	var dot_product = new_heading.dot(velocity.normalized())
+	var traction = traction_curve.sample(linear_velocity.x / engine_power)
+	var dot_product = new_heading.dot(linear_velocity.normalized())
 	
 	if dot_product > 0:
-		velocity = velocity.lerp(new_heading * velocity.length(), traction)
+		linear_velocity = linear_velocity.lerp(new_heading * linear_velocity.length(), traction)
 	if dot_product < 0:
-		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+		linear_velocity = -new_heading * min(linear_velocity.length(), max_speed_reverse)
 	
 	#this is the source of the sudden turns, but I don't know exactly how to smooth it out
 	#rotation = new_heading.angle()
-	var new_angle: Vector2 = Vector2(lerp(new_heading.x, last_heading.x, traction),
-	lerp(new_heading.y, last_heading.y, traction))
 
-	rotation = new_angle.angle()
+	rotation = new_heading.angle()
 	last_heading = new_heading
 	
 
