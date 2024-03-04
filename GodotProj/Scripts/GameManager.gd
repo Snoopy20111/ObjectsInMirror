a1fba83f02b672db21b063ad9c51f2b8d152ea4f
Snoopy20111 <snoopy20111@gmail.com>
2024-03-04 12:35:33 -0600

    Screen Space Shaders
    Added a system for controlling a few full-screen shaders (Vignette, Rain, and Chromatic Abberation, the last of which I haven't written yet but is set up and ready).
    
    Set up under Game Manager with a system for fiddling with their parameters at runtime too.

--- GodotProj/Scripts/GameManager.gd
+++ GodotProj/Scripts/GameManager.gd
@@ -1,11 +1,25 @@
 extends Node
 
 var is_paused: bool = false
+@export var fullscreen_effects: Array
 
 ### Specific Utilities ###
 func retarget_camera2D(_new_target: Node2D):
 	#Set camera to target this object
 	pass
+
+func setFullscreenShaderParam(effect: int, param_name: String, new_value):
+	if (effect > fullscreen_effects.size()):
+		push_error("effect value outside array range")
+		return
+	if (new_value == null):
+		push_error("value given was null")
+		return
+		
+	# Don't know of any way to error-check names and types of shader params,
+	# So we just have to hope Godot will throw meaningful errors
+	var my_material = fullscreen_effects[effect].get_material(0)
+	my_material.set_shader_param(param_name, new_value)
 
 ### Utilities ###
 func reparent_other(child:Node, new_parent:Node) -> void:
--- GodotProj/Scripts/myEnums.gd
+++ GodotProj/Scripts/myEnums.gd
@@ -4,6 +4,7 @@ class_name Enums
 # Camera & Visual Enums
 #enum CAM_MOVE_STATE {DRAG, ZOOM, SCROLL}
 enum FADE_STATE {IN, PAUSE, OUT}
+enum CANVAS_EFFECT {VIGNETTE, CHROMATIC_ABB, RAIN}
 
 #Gameplay Enums
 #enum GEAR {REVERSE, FIRST, SECOND, THIRD, FOURTH}
