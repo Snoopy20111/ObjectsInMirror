2fd28f1a5979f22169443b42da4f56297073027e
Snoopy20111 <snoopy20111@gmail.com>
2024-03-05 16:44:00 -0600

    Cine_Intro_01 fully working
    With dialogue and lighting and set dressing and all! Even loads the second scene, although we're having an error with Phantom Camera (not sure how to make it work yet)

--- GodotProj/Scripts/Cine_Intro_01_Road.gd
+++ GodotProj/Scripts/Cine_Intro_01_Road.gd
@@ -6,17 +6,30 @@ extends Node2D
 @export var loopingEnv_thresholdY: float = 2048
 @export var loopingEnv_speed: float = 300
 
+@export var dialogue_Intro_01: DialogueResource = load("res://Dialogue/Cine_Intro_01.dialogue")
+@export var dialogue_Intro_02: DialogueResource = load("res://Dialogue/Cine_Intro_02.dialogue")
+
+@export var sceneToLoad: String = "res://Scenes/Cine_Intro_02_GasStation.tscn"
+
 @onready var loopingEnv: Node2D = $LoopingEnv
 @onready var loopingEnv2: Node2D = $LoopingEnv2
 @onready var timerToFirstDialogue: Timer = $Timers/Timer_ToFirstDialogue
+@onready var timerToSecondDialogue: Timer = $Timers/Timer_ToSecondDialogue
+@onready var timerToExit: Timer = $Timers/Timer_ToExit
 
-#func _ready():
-	#pass
+var currentDialogue: int = 0
 
+
+func _ready():
+	# Connect dialogue manager signals
+	DialogueManager.dialogue_ended.connect(dia_end)
+	
+	# Set Fullscreen shaders: vignette and rain, if not already on
+	GameManager.setFullscreenShaderActive(Enums.CANVAS_EFFECT.VIGNETTE, true)
+	GameManager.setFullscreenShaderActive(Enums.CANVAS_EFFECT.RAIN, true)
 
 func _process(delta):
 	loop_environment(delta)
-
 
 func loop_environment(delta: float) -> void:
 	if (loopingEnv.position.y >= loopingEnv_thresholdY):
@@ -25,3 +38,23 @@ func loop_environment(delta: float) -> void:
 	if (loopingEnv2.position.y >= loopingEnv_thresholdY):
 		loopingEnv2.position.y -= loopingEnv_loopDistance
 	loopingEnv2.position.y += delta * loopingEnv_speed
+
+
+#Disgusting custom script, chain reaction of signals pinging back and forth
+func _on_timer_to_first_dialogue_timeout():
+	DialogueManager.show_example_dialogue_balloon(dialogue_Intro_01, "start")
+	currentDialogue += 1
+
+func _on_timer_to_second_dialogue_timeout():
+	currentDialogue += 1
+	DialogueManager.show_dialogue_balloon(dialogue_Intro_02, "start")
+
+func _on_timer_to_exit_timeout():
+	SceneManager.change_scene(sceneToLoad)
+
+func dia_end(_resource: DialogueResource):
+	match (currentDialogue):
+		1:
+			timerToSecondDialogue.start()
+		2:
+			timerToExit.start()
