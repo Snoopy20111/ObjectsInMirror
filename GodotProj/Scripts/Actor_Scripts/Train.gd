2cf30b7d1fafd4c01feff168becdb5653c33be93
Snoopy20111 <snoopy20111@gmail.com>
2024-03-24 00:11:19 -0500

    Train Audio Pt1
    It kinda sucks right now, but it'll get better. At least it triggers and plays at all.

--- GodotProj/Scripts/Actor_Scripts/Train.gd
+++ GodotProj/Scripts/Actor_Scripts/Train.gd
@@ -5,13 +5,38 @@ class_name Train
 var selfControlling: bool = false
 var speed: float = 10.0
 
+@onready var trainPassbyEmitter = $AudioEmitters/ChuggaChugga
+@onready var trainHornEmitter = $AudioEmitters/ChooChoo
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+func _ready():
+	Wwise.register_game_obj(trainHornEmitter, "Train Horn")
+	Wwise.register_game_obj(trainPassbyEmitter, "Train Chug")
+	Wwise.set_2d_position(trainHornEmitter, trainHornEmitter.global_transform, 0)
+	Wwise.set_2d_position(trainPassbyEmitter, trainPassbyEmitter.global_transform, 0)
+
+func _process(_delta):
+	Wwise.set_2d_position(trainHornEmitter, trainHornEmitter.global_transform, 0)
+	Wwise.set_2d_position(trainPassbyEmitter, trainPassbyEmitter.global_transform, 0)
+	#print(trainHornEmitter.global_transform);
+
 func _physics_process(delta):
 	if (selfControlling):
 		progress += speed * delta
 
 
-func _on_collision_body_entered(body):
+func _on_collision_body_entered(body) -> void:
 	if (body is Entity_Chaser):
 		body.remove_entity()
+
+func activate_train() -> void:
+	if (!selfControlling):
+		selfControlling = true
+		Wwise.post_event("ACTR_Train_Horn", trainHornEmitter)
+		Wwise.post_event("ACTR_Train_Play", trainPassbyEmitter)
+		#print("Should be playing sound")
+
+func _exit_tree():
+	Wwise.post_event("ACTR_Train_Stop", trainPassbyEmitter)
+	Wwise.unregister_game_obj(trainHornEmitter)
+	Wwise.unregister_game_obj(trainPassbyEmitter)
--- GodotProj/Scripts/Trigger_Train_Boss.gd
+++ GodotProj/Scripts/Trigger_Train_Boss.gd
@@ -8,12 +8,6 @@ var isTracking: bool = false
 var _speedAvg: float = 0.0
 
 
-# Called when the node enters the scene tree for the first time.
-#func _ready():
-	#pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if (isTracking):
 		var temp: float = abs(playerCar.linear_velocity.y)
@@ -21,13 +15,11 @@ func _physics_process(delta):
 		_speedAvg -= _speedAvg / speedAvgSize
 		_speedAvg += (temp / speedAvgSize)
 
-
 func _on_trigger_train_start_player_crossed():
 	isTracking = true
-
+	trainNode.activate_train()
 
 func _on_trigger_train_end_player_crossed():
 	# Release the train to keep going past at its average speed so far
 	isTracking = false
-	trainNode.selfControlling = true
 	trainNode.speed = _speedAvg
